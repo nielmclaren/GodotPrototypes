e3b1de42482fcce6b244337ad40078730ac129c6
Niel McLaren <d@nielmclaren.com>
2025-04-17 12:10:25 -0700

    Drag blocks from where they were clicked.

--- PositioningBlocks/block.gd
+++ PositioningBlocks/block.gd
@@ -3,17 +3,18 @@ extends RigidBody2D
 signal clicked
 
 var held = false
+var click_offset = Vector2.ZERO
 
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
 			and event.button_index == MOUSE_BUTTON_LEFT \
 			and event.pressed:
-		print("clicked")
+		click_offset = get_global_mouse_position() - global_transform.origin
 		clicked.emit(self)
 
 func _physics_process(_delta):
 	if held:
-		global_transform.origin = get_global_mouse_position()
+		global_transform.origin = get_global_mouse_position() - click_offset
 
 func pickup():
 	if held:
@@ -26,3 +27,4 @@ func drop(impulse=Vector2.ZERO):
 		freeze = false
 		apply_central_impulse(impulse)
 		held = false
+		click_offset = Vector2.ZERO
