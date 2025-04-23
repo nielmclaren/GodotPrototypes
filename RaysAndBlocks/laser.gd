4a3c5170303ab24b4856b9aff463a25fb06fc2d0
Niel McLaren <d@nielmclaren.com>
2025-04-23 11:56:19 -0700

    Add debug art. Add refraction.

--- RaysAndBlocks/laser.gd
+++ RaysAndBlocks/laser.gd
@@ -5,6 +5,7 @@ class_name Laser
 const DEFAULT_COLLISION_LAYER:int = 1
 const REVERSE_CAST_COLLISION_LAYER:int = 2
 const MAX_LASER_DEPTH:int = 5
+const REFRACTIVE_INDEX_GLASS:float = 1.52
 
 var laser_scene:PackedScene
 var child_laser:Laser
@@ -27,6 +28,9 @@ func _physics_process(_delta:float) -> void:
 	# Indicate depth and external vs. internal
 	($DepthLabel as Label).text = str(laser_depth) + ("i" if containing_block else "x")
 
+	var green_line:Line2D = $GreenLine
+	green_line.hide()
+
 	if containing_block:
 		_process_internal_ray()
 
@@ -41,7 +45,8 @@ func _process_internal_ray() -> void:
 	reverse_cast.force_raycast_update()
 	if reverse_cast.is_colliding():
 		var cast_point:Vector2 = to_local(reverse_cast.get_collision_point())
-		_update_art(cast_point)
+		var collision_normal:Vector2 = reverse_cast.get_collision_normal()
+		_update_art(cast_point, collision_normal)
 
 		if not child_laser:
 			child_laser = _instantiate_laser(laser_depth + 1)
@@ -52,7 +57,7 @@ func _process_internal_ray() -> void:
 		# Child laser will be outside the block.
 		child_laser.set_containing_block(null)
 
-		_update_child_laser(cast_point)
+		_update_child_laser(cast_point, reverse_cast.get_collision_normal().rotated(PI), true)
 	else:
 		print("WARN: reverse raycast for internal laser didn't collide.")
 
@@ -60,16 +65,18 @@ func _process_internal_ray() -> void:
 			child_laser.queue_free()
 			child_laser = null
 
-		_update_art(Vector2.ZERO)
+		_update_art(Vector2.ZERO, Vector2.ZERO)
 
 	containing_block.set_collision_layer_value(REVERSE_CAST_COLLISION_LAYER, false)
 
 func _process_external_ray() -> void:
 	var cast_point:Vector2 = target_position
+	var normal:Vector2 = Vector2.ZERO
 
 	force_raycast_update()
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
+		normal = get_collision_normal()
 
 		if not child_laser:
 			child_laser = _instantiate_laser(laser_depth + 1)
@@ -80,13 +87,13 @@ func _process_external_ray() -> void:
 		# Child laser will be inside the block.
 		child_laser.set_containing_block(get_collider() as Block)
 
-		_update_child_laser(cast_point)
+		_update_child_laser(cast_point, get_collision_normal(), false)
 
 	elif child_laser:
 		child_laser.queue_free()
 		child_laser = null
 
-	_update_art(cast_point)
+	_update_art(cast_point, normal)
 
 func set_containing_block(block:Block) -> void:
 	containing_block = block
@@ -128,11 +135,30 @@ func _instantiate_laser(depth:int) -> Laser:
 	add_child(laser)
 	return laser
 
-func _update_child_laser(start_position:Vector2) -> void:
-	child_laser.position = start_position
-	child_laser.rotation = rotation + PI/6
+func _update_child_laser(point:Vector2, normal:Vector2, is_internal:bool) -> void:
+	child_laser.position = point
 
-func _update_art(target_point:Vector2) -> void:
+	var direction:Vector2 = Vector2.from_angle(global_rotation)
+	var reverse_normal:Vector2 = normal.rotated(PI)
+	var in_angle:float = reverse_normal.angle_to(direction)
+
+	var out_angle:float
+	if is_internal:
+		out_angle = asin( REFRACTIVE_INDEX_GLASS * sin(in_angle) )
+	else:
+		out_angle = asin( sin(in_angle) / REFRACTIVE_INDEX_GLASS )
+
+	child_laser.global_rotation = reverse_normal.rotated(out_angle).angle()
+
+func _update_art(target_point:Vector2, normal:Vector2) -> void:
 	var line:Line2D = $Line2D
 	line.points[1] = target_point
 	line.show()
+
+	var green_line:Line2D = $GreenLine
+	if normal.length() > 0:
+		green_line.points[0] = target_point
+		green_line.points[1] = target_point + normal.rotated(-global_rotation) * 50
+		green_line.show()
+	else:
+		green_line.hide()
