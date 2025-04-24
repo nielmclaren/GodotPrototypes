bb1a143e2acca1c3bab093f13b345345014e24ed
Niel McLaren <d@nielmclaren.com>
2025-04-23 17:10:50 -0700

    Stop the laser beam when theta is above the critical angle.

--- RaysAndBlocks/laser.gd
+++ RaysAndBlocks/laser.gd
@@ -45,19 +45,27 @@ func _process_internal_ray() -> void:
 	reverse_cast.force_raycast_update()
 	if reverse_cast.is_colliding():
 		var cast_point:Vector2 = to_local(reverse_cast.get_collision_point())
-		var collision_normal:Vector2 = reverse_cast.get_collision_normal()
-		_update_art(cast_point, collision_normal)
+		_update_art(cast_point, reverse_cast.get_collision_normal())
 
-		if not child_laser:
-			child_laser = _instantiate_laser(laser_depth + 1)
+		var normal:Vector2 = reverse_cast.get_collision_normal().rotated(PI)
+		var angle_of_refraction:float = _get_angle_of_refraction(normal, true)
 
-		child_laser.clear_exceptions()
-		child_laser.add_exception(containing_block as CollisionObject2D)
+		if PI/2 - angle_of_refraction < deg_to_rad(3):
+			if child_laser:
+				child_laser.queue_free()
+				child_laser = null
+		else:
+			if not child_laser:
+				child_laser = _instantiate_laser(laser_depth + 1)
 
-		# Child laser will be outside the block.
-		child_laser.set_containing_block(null)
+			child_laser.clear_exceptions()
+			child_laser.add_exception(containing_block as CollisionObject2D)
 
-		_update_child_laser(cast_point, reverse_cast.get_collision_normal().rotated(PI), true)
+			# Child laser will be outside the block.
+			child_laser.set_containing_block(null)
+
+			child_laser.position = cast_point
+			child_laser.global_rotation = _get_child_laser_global_rotation(normal, true)
 	else:
 		print("WARN: reverse raycast for internal laser didn't collide.")
 
@@ -87,7 +95,8 @@ func _process_external_ray() -> void:
 		# Child laser will be inside the block.
 		child_laser.set_containing_block(get_collider() as Block)
 
-		_update_child_laser(cast_point, get_collision_normal(), false)
+		child_laser.position = cast_point
+		child_laser.global_rotation = _get_child_laser_global_rotation(get_collision_normal(), false)
 
 	elif child_laser:
 		child_laser.queue_free()
@@ -135,20 +144,20 @@ func _instantiate_laser(depth:int) -> Laser:
 	add_child(laser)
 	return laser
 
-func _update_child_laser(point:Vector2, normal:Vector2, is_internal:bool) -> void:
-	child_laser.position = point
-
+func _get_angle_of_refraction(normal:Vector2, is_internal:bool) -> float:
 	var direction:Vector2 = Vector2.from_angle(global_rotation)
 	var reverse_normal:Vector2 = normal.rotated(PI)
 	var in_angle:float = reverse_normal.angle_to(direction)
 
-	var out_angle:float
 	if is_internal:
-		out_angle = asin( REFRACTIVE_INDEX_GLASS * sin(in_angle) )
-	else:
-		out_angle = asin( sin(in_angle) / REFRACTIVE_INDEX_GLASS )
+		return asin( sin(in_angle) * REFRACTIVE_INDEX_GLASS  )
 
-	child_laser.global_rotation = reverse_normal.rotated(out_angle).angle()
+	return asin( sin(in_angle) / REFRACTIVE_INDEX_GLASS )
+
+func _get_child_laser_global_rotation(normal:Vector2, is_internal:bool) -> float:
+	var reverse_normal:Vector2 = normal.rotated(PI)
+	var out_angle:float = _get_angle_of_refraction(normal, is_internal)
+	return reverse_normal.rotated(out_angle).angle()
 
 func _update_art(target_point:Vector2, normal:Vector2) -> void:
 	var line:Line2D = $Line2D
@@ -158,7 +167,7 @@ func _update_art(target_point:Vector2, normal:Vector2) -> void:
 	var green_line:Line2D = $GreenLine
 	if normal.length() > 0:
 		green_line.points[0] = target_point
-		green_line.points[1] = target_point + normal.rotated(-global_rotation) * 50
+		green_line.points[1] = target_point + normal.rotated(-global_rotation) * 20
 		green_line.show()
 	else:
 		green_line.hide()
