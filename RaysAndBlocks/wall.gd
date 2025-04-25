40feee58c87797e75eb663bc9a17cbb08a154d1e
Niel McLaren <d@nielmclaren.com>
2025-04-25 16:15:32 -0700

    Walls and emitters can no longer be dragged or pushed around.

--- RaysAndBlocks/wall.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody2D
-
-class_name Wall
-
-func _input_event(viewport:Viewport, event:InputEvent, shape_idx:int) -> void:
-	($Draggable as Draggable).input_event(viewport, event, shape_idx)
