54778a64754cb3e459768b02649152c109ba8eb2
Niel McLaren <d@nielmclaren.com>
2025-04-25 17:24:40 -0700

    Support multiple emitters by placing Laser scene inside Emitter scene.

--- RaysAndBlocks/emitter.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-class_name Emitter
-
-func _ready() -> void:
-	var follow:PathFollow2D = $Path2D/PathFollow2D
-	follow.set_progress_ratio(0.0)
--- RaysAndBlocks/main.gd
+++ RaysAndBlocks/main.gd
@@ -1,20 +1,5 @@
 extends Node2D
 
-@export var laser_scene: PackedScene
-
-var laser:Laser = null
-
-func _ready() -> void:
-	laser = laser_scene.instantiate()
-	laser.add_exception($Emitter as CollisionObject2D)
-	add_child(laser)
-
-func _process(_delta:float) -> void:
-	var emitter:PathFollow2D = $Emitter/Path2D/PathFollow2D
-	laser.position = emitter.global_position
-	laser.rotation = emitter.global_rotation
-
-
 func _unhandled_input(event:InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var mouse_event:InputEventMouseButton = event
