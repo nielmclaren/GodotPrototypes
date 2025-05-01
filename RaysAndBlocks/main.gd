6638da9278655317280ace2f44920033081a34ca
Niel McLaren <d@nielmclaren.com>
2025-05-01 12:26:43 -0700

    Use an editor script to add wall outlines.

--- RaysAndBlocks/main.gd
+++ RaysAndBlocks/main.gd
@@ -1,21 +1,10 @@
 extends Node2D
 
-var outline_scene:PackedScene
-
 var drag_and_drop: DragAndDrop
 
 func _ready() -> void:
 	drag_and_drop = DragAndDrop.new()
 	drag_and_drop.init(self)
-
-	outline_scene = load("res://wall_01_outline.tscn") as PackedScene
-
-	var outline_parent: Node2D = $WallOutlines
-	var wall_parent: Node2D = $Walls
-	for node: Node2D in wall_parent.get_children():
-		var outline: Node2D = outline_scene.instantiate()
-		outline.transform = node.transform
-		outline_parent.add_child(outline)
 
 func _physics_process(delta: float) -> void:
 	drag_and_drop.physics_process(delta)
